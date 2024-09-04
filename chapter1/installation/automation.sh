a7cd55fadafaf001d1795ebd0b75a33b5e61d4b3
Eliaz Simon <eliaz5536@gmail.com>
2024-09-04 23:22:36 +0100

    Create automation.sh

---
+++ chapter1/installation/automation.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Perform this for all Linux Distributions, such as Fedora, Debian, gentoo and so fourth
+
+# For Debian#
+sudo apt-get install coreutils
+sudo apt-get install bash
+sudo apt-get install binutils
+sudo apt-get install bison
+sudo apt-get install diffiutils
+sudo apt-get install findutils
+sudo apt-get install gawk
+sudo apt-get install gcc
+sudo apt-get install g++
+sudo apt-get install grep
+sudo apt-get install gzip
+sudo apt-get install m4
+sudo apt-get install make
+sudo apt-get install patch
+sudo apt-get install perl
+sudo apt-get install python3
+sudo apt-get install sed
+sudo apt-get install tar 
+sudo apt-get install texinfo
+sudo apt-get install xz-utils
+
+# Check kernel
