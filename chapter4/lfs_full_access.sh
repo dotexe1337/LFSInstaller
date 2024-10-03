52fcda6332b80db80cfa0180b1dbe1ff31a7c24b
Eliaz Simon <eliaz5536@gmail.com>
2024-10-03 22:13:02 +0100

    Delete chapter4 directory

--- chapter4/lfs_full_access.sh
+++
@@ -1,4 +0,0 @@
-chown -v lfs $LFS/{usr{,/*},lib,var,etc,bin,sbin,tools}
-case $(uname -m) in
-  x86_64) chown -v lfs $LFS/lib64 ;;
-esac
--- chapter4/rename_certain_directories.sh
+++
@@ -1,3 +0,0 @@
-mv -v mpfr-4.2.1 mpfr
-mv -v gmp-6.3.0 gmp
-mv -v mpc-1.3.1 mpc
