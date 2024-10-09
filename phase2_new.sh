1482ad053cf806ad6deffdb15da8819ccda01e34
Eliaz Simon <eliaz5536@gmail.com>
2024-10-09 17:49:07 +0100

    Update phase2_new.sh

--- phase2_new.sh
+++ phase2_new.sh
@@ -21,18 +21,14 @@ case $(uname -m) in
   x86_64) chown -v lfs $LFS/lib64 ;;
 esac
 
-chown lfs:lfs $LFS/sources/*
+# chown lfs:lfs $LFS/sources/*
 
 # Start a shell running as user lfs. This can be done by logging in as lfs on a virtual console or with the following substitute/switch user command:
 su - lfs <<EOF
-
-# 4.4. Setting Up the Environment
-# Set up a good working environment by creating two new startup files for the bash shell. While logged in as user lfs, issue the following command to create a new .bash_profile:
 cat > ~/.bash_profile << "END"
 exec env -i HOME=$HOME TERM=$TERM PS1='\u:\w\$ ' /bin/bash
 END
 
-# The new instance of the shell is a non-login shell, which does not read and execute the contents of the /etc/profile or .bash_profile files, but rather reads and executes the .bashrc file instead. Create the .bashrc file now:
 cat > ~/.bashrc << "END"
 set +h
 umask 022
@@ -46,20 +42,10 @@ CONFIG_SITE=$LFS/usr/share/config.site
 export LFS LC_ALL LFS_TGT PATH CONFIG_SITE
 END
 
-# To ensure the environment is fully prepared for building the temporary tools, force the bash shell to read the new user profile:
-#source ~/.bash_profile
+source ~/.bash_profile
+source ~/.bashrc
 
-# iii. General Compilation Instructions
-export LFS=/mnt/lfs
-export LC_ALL=POSIX
-export LFS_TGT=$(uname -m)-lfs-linux-gnu
-export PATH=$LFS/tools/bin:/usr/bin:/bin
-export CONFIG_SITE=$LFS/usr/share/config.site
-
-# ===========================================================================
-# Chapter 5. Compiling a Cross-Toolchain
-# ===========================================================================
-# 5.2. Binutils-2.43.1 - Pass 1
+5.2. Binutils-2.43.1 - Pass 1
 cd /mnt/lfs/sources/binutils-2.43.1
 
 mkdir -v build
@@ -77,7 +63,7 @@ cd       build
 make
 make install
 
-# 5.3. GCC-14.2.0 - Pass 1
+
 cd /mnt/lfs/sources/gcc-14.2.0
 
 tar -xf ../mpfr-4.2.1.tar.xz
@@ -125,7 +111,7 @@ cd ..
 cat gcc/limitx.h gcc/glimits.h gcc/limity.h > \
   `dirname $($LFS_TGT-gcc -print-libgcc-file-name)`/include/limits.h
 
-# 5.4. Linux-6.10.5 API Headers
+
 cd /mnt/lfs/sources/linux-6.10.5
 
 make mrproper
@@ -134,7 +120,7 @@ make headers
 find usr/include -type f ! -name '*.h' -delete
 cp -rv usr/include $LFS/usr
 
-# 5.5. Glibc-2.40
+
 cd /mnt/lfs/sources/glibc-2.40
 
 case $(uname -m) in
@@ -150,8 +136,7 @@ patch -Np1 -i ../glibc-2.40-fhs-1.patch
 mkdir -v build
 cd       build
 
-# echo "rootsbindir=/usr/sbin" > configparms
-echo "rootsbindir=$LFS/usr/sbin" > configparms
+echo "rootsbindir=/usr/sbin" > configparms
 
 ../configure                             \
       --prefix=$LFS/usr                  \
@@ -173,7 +158,7 @@ readelf -l a.out | grep ld-linux
 
 rm -v a.out
 
-# 5.6. Libstdc++ from GCC-14.2.0
+
 cd /mnt/lfs/sources/gcc-14.2.0
 
 mkdir -v build
@@ -182,7 +167,7 @@ cd       build
 ../libstdc++-v3/configure           \
     --host=$LFS_TGT                 \
     --build=$(../config.guess)      \
-    --prefix=$LFS/usr               \
+    --prefix=/usr               \
     --disable-multilib              \
     --disable-nls                   \
     --disable-libstdcxx-pch         \
@@ -192,12 +177,10 @@ make
 make DESTDIR=$LFS install
 rm -v $LFS/usr/lib/lib{stdc++{,exp,fs},supc++}.la
 
-# ===========================================================================
-# Chapter 6. Cross Compiling Temporary Tools
-# ===========================================================================
+
 # 6.2. M4-1.4.19
 cd /mnt/lfs/sources/m4-1.4.19
-./configure --prefix=$LFS/usr   \
+./configure --prefix=/usr   \
             --host=$LFS_TGT \
             --build=$(build-aux/config.guess)
 make
@@ -215,7 +198,7 @@ pushd build
   make -C progs tic
 popd
 
-./configure --prefix=$LFS/usr            \
+./configure --prefix=/usr                \
             --host=$LFS_TGT              \
             --build=$(./config.guess)    \
             --mandir=/usr/share/man      \
@@ -234,10 +217,10 @@ ln -sv libncursesw.so $LFS/usr/lib/libncurses.so
 sed -e 's/^#if.*XOPEN.*$/#if 1/' \
     -i $LFS/usr/include/curses.h
 
-# 6.4. Bash-5.2.32
+
 cd /mnt/lfs/sources/bash-5.2.32
 
-./configure --prefix=$LFS/usr                  \
+./configure --prefix=/usr                      \
             --build=$(sh support/config.guess) \
             --host=$LFS_TGT                    \
             --without-bash-malloc              \
@@ -247,10 +230,11 @@ make
 make DESTDIR=$LFS install
 ln -sv bash $LFS/bin/sh
 
+
 # 6.5. Coreutils-9.5
 cd /mnt/lfs/sources/coreutils-9.5
 
-./configure --prefix=$LFS/usr                 \
+./configure --prefix=/usr                     \
             --host=$LFS_TGT                   \
             --build=$(build-aux/config.guess) \
             --enable-install-program=hostname \
@@ -265,10 +249,10 @@ mkdir -pv $LFS/usr/share/man/man8
 mv -v $LFS/usr/share/man/man1/chroot.1 $LFS/usr/share/man/man8/chroot.8
 sed -i 's/"1"/"8"/'                    $LFS/usr/share/man/man8/chroot.8
 
-# 6.6. Diffutils-3.10
+
 cd /mnt/lfs/sources/diffutils-3.10
 
-./configure --prefix=$LFS/usr \
+./configure --prefix=/usr \
             --host=$LFS_TGT \
             --build=$(./build-aux/config.guess)
 
@@ -276,7 +260,7 @@ make
 
 make DESTDIR=$LFS install
 
-# 6.7. File-5.45
+
 cd /mnt/lfs/sources/file-5.45
 
 mkdir build
@@ -288,7 +272,7 @@ pushd build
   make
 popd
 
-./configure --prefix=$LFS/usr --host=$LFS_TGT --build=$(./config.guess)
+./configure --prefix=/usr --host=$LFS_TGT --build=$(./config.guess)
 
 make FILE_COMPILE=$(pwd)/build/src/file
 
@@ -296,10 +280,11 @@ make DESTDIR=$LFS install
 
 rm -v $LFS/usr/lib/libmagic.la
 
-# 6.8. Findutils-4.10.0
+
+
 cd /mnt/lfs/sources/findutils-4.10.0
 
-./configure --prefix=$LFS/usr                   \
+./configure --prefix=/usr                   \
             --localstatedir=/var/lib/locate \
             --host=$LFS_TGT                 \
             --build=$(build-aux/config.guess)
@@ -308,12 +293,13 @@ make
 
 make DESTDIR=$LFS install
 
-# 6.9. Gawk-5.3.0
+
+
 cd /mnt/lfs/sources/gawk-5.3.0
 
 sed -i 's/extras//' Makefile.in
 
-./configure --prefix=$LFS/usr   \
+./configure --prefix=/usr   \
             --host=$LFS_TGT \
             --build=$(build-aux/config.guess)
 
@@ -321,10 +307,13 @@ make
 
 make DESTDIR=$LFS install
 
+
+
+
 # 6.10. Grep-3.11
 cd /mnt/lfs/sources/grep-3.11
 
-./configure --prefix=$LFS/usr   \
+./configure --prefix=/usr   \
             --host=$LFS_TGT \
             --build=$(./build-aux/config.guess)
 
@@ -332,19 +321,23 @@ make
 
 make DESTDIR=$LFS install
 
+
+
 # 6.11. Gzip-1.13
 cd /mnt/lfs/sources/gzip-1.13
 
-./configure --prefix=$LFS/usr --host=$LFS_TGT
+./configure --prefix=/usr --host=$LFS_TGT
 
 make
 
 make DESTDIR=$LFS install
 
+
+
 # 6.12. Make-4.4.1
 cd /mnt/lfs/sources/make-4.4.1
 
-./configure --prefix=$LFS/usr   \
+./configure --prefix=/usr   \
             --without-guile \
             --host=$LFS_TGT \
             --build=$(build-aux/config.guess)
@@ -353,10 +346,12 @@ make
 
 make DESTDIR=$LFS install
 
+
+
 # 6.13. Patch-2.7.6
 cd /mnt/lfs/sources/patch-2.7.6
 
-./configure --prefix=$LFS/usr   \
+./configure --prefix=/usr   \
             --host=$LFS_TGT \
             --build=$(build-aux/config.guess)
 
@@ -364,10 +359,12 @@ make
 
 make DESTDIR=$LFS install
 
+
+
 # 6.14. Sed-4.9
 cd /mnt/lfs/sources/sed-4.9
 
-./configure --prefix=$LFS/usr   \
+./configure --prefix=/usr   \
             --host=$LFS_TGT \
             --build=$(./build-aux/config.guess)
 
@@ -375,10 +372,12 @@ make
 
 make DESTDIR=$LFS install
 
+
+
 # 6.15. Tar-1.35
 cd /mnt/lfs/sources/tar-1.35
 
-./configure --prefix=$LFS/usr                     \
+./configure --prefix=/usr                     \
             --host=$LFS_TGT                   \
             --build=$(build-aux/config.guess)
 
@@ -386,10 +385,12 @@ make
 
 make DESTDIR=$LFS install
 
+
+
 # 6.16. Xz-5.6.2
 cd /mnt/lfs/sources/xz-5.6.2
 
-./configure --prefix=$LFS/usr                     \
+./configure --prefix=/usr                     \
             --host=$LFS_TGT                   \
             --build=$(build-aux/config.guess) \
             --disable-static                  \
@@ -401,7 +402,8 @@ make DESTDIR=$LFS install
 
 rm -v $LFS/usr/lib/liblzma.la
 
-# 6.17. Binutils-2.43.1 - Pass 2
+
+
 cd /mnt/lfs/sources/binutils-2.43.1
 
 sed '6009s/$add_dir//' -i ltmain.sh
@@ -410,7 +412,7 @@ mkdir -v build
 cd       build
 
 ../configure                   \
-    --prefix=$LFS/usr          \
+    --prefix=/usr              \
     --build=$(../config.guess) \
     --host=$LFS_TGT            \
     --disable-nls              \
@@ -426,6 +428,8 @@ make
 make DESTDIR=$LFS install
 
 rm -v $LFS/usr/lib/lib{bfd,ctf,ctf-nobfd,opcodes,sframe}.{a,la}
+
+
 
 # 6.18. GCC-14.2.0 - Pass 2
 cd /mnt/lfs/sources/gcc-14.2.0
@@ -455,7 +459,7 @@ cd       build
     --host=$LFS_TGT                                \
     --target=$LFS_TGT                              \
     LDFLAGS_FOR_TARGET=-L$PWD/$LFS_TGT/libgcc      \
-    --prefix=$LFS/usr                              \
+    --prefix=/usr                                  \
     --with-build-sysroot=$LFS                      \
     --enable-default-pie                           \
     --enable-default-ssp                           \
