0c8dd69895d3aefe4e3228a93e26f80fd9b4599b
Eliaz Simon <eliaz5536@gmail.com>
2024-10-13 03:00:27 +0100

    Add files via upload

---
+++ LFSInstaller.sh
@@ -0,0 +1,369 @@
+#!/bin/bash
+#================================================================
+# HEADER 
+#================================================================
+# Script Name    : LFSInstaller.sh
+# Description    : Complete LFS Installer
+# Author         : Eliaz Simon
+# Date           : 2024-10-13
+# Version        : 1.0
+# Usage          : ./LFSInstaller.sh
+# Notes          : 
+# Dependencies   : 
+#================================================================
+# MIT License
+# Copyright (c) 2024 Eliaz Simon
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this
+# software and associated documentation files (the Software), to deal in the Software
+# without restriction, including without limitation the rights to use, copy, modify, merge,
+# publish, distribute, sublicense, and/or sell copies of the Software, and to permit
+# persons to whom the Software is furnished to do so, subject to the following
+# conditions:
+#
+# The above copyright notice and this permission notice shall be included in all copies or
+# substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED AS IS, WITHOUT WARRANTY OF ANY KIND, EXPRESS
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+# CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#================================================================
+
+#################################################################
+# ANSI Color Code Escape Sequences			        #
+#################################################################
+
+# Colors
+BLACK="\e[30m"
+GREEN="\e[32m"
+YELLOW="\e[33m"
+BLUE="\e[34m"
+MAGENTA="\e[35m"
+CYAN="\e[36m"
+LIGHT_GRAY="\e[37m"
+GRAY="\e[90m"
+LIGHT_RED="\e[91m"
+LIGHT_GREEN="\e[92m"
+LIGHT_YELLOW="\e[93m"
+LIGHT_BLUE="\e[94m"
+LIGHT_MAGENTA="\e[95m"
+LIGHT_CYAN="\e[96m"
+
+# Background Colors
+BLACK_BG="\e[40m"
+RED_BG="\e[41m"
+GREEN_BG="\e[42m"
+YELLOW_BG="\e[43m"
+BLUE_BG="\e[44m"
+MAGENTA_BG="\e[45m"
+CYAN_BG="\e[46m"
+WHITE_BG="\e[47m"
+
+# Bold
+BOLD_BLACK="\e[1;30m"
+BOLD_RED="\e[1;31m"
+BOLD_GREEN="\e[1;32m"
+BOLD_YELLOW="\e[1;33m"
+BOLD_BLUE="\e[1;34m"
+BOLD_MAGENTA="\e[1;35m"
+BOLD_CYAN="\e[1;36m"
+BOLD_LIGHT_GRAY="\e[1;37m"
+BOLD_GRAY="\e[1;90m"
+BOLD_LIGHT_RED="\e[1;91m"
+BOLD_LIGHT_GREEN="\e[1;92m"
+BOLD_LIGHT_YELLOW="\e[1;93m"
+BOLD_LIGHT_BLUE="\e[1;94m"
+BOLD_LIGHT_MAGENTA="\e[1;95m"
+BOLD_LIGHT_CYAN="\e[1;96m"
+BOLD_WHITE="\e[1;97m"
+
+# Italic
+ITALIC_BLACK="\e[3;30m"
+ITALIC_RED="\e[3;31m"
+ITALIC_GREEN="\e[3;32m"
+ITALIC_YELLOW="\e[3;33m"
+ITALIC_BLUE="\e[3;34m"
+ITALIC_MAGENTA="\e[3;35m"
+ITALIC_CYAN="\e[3;36m"
+ITALIC_LIGHT_GRAY="\e[3;37m"
+ITALIC_GRAY="\e[3;90m"
+ITALIC_LIGHT_RED="\e[3;91m"
+ITALIC_LIGHT_GREEN="\e[3;92m"
+ITALIC_LIGHT_YELLOW="\e[3;93m"
+ITALIC_LIGHT_BLUE="\e[3;94m"
+ITALIC_LIGHT_MAGENTA="\e[3;95m"
+ITALIC_LIGHT_CYAN="\e[3;96m"
+ITALIC_WHITE="\e[3;97m"
+
+# Styles
+BOLD="\e[1m"
+ITALIC="\e[3m"
+
+ENDCOLOR="\e[0m"
+
+
+#================================================================
+# FUNCTION: info
+# DESCRIPTION:
+#     Prints info message using the provided parameter.
+# PARAMETERS:
+#     $1 - A message
+# RETURNS:
+#     None
+#================================================================
+info() {
+	local message=$1
+	printf "${WHITE}[${ENDCOLOR}${CYAN}$current_time${ENDCOLOR}${WHITE}]${ENDCOLOR} ${WHITE}[${ENDCOLOR}${GREEN}INFO${ENDCOLOR}${WHITE}]${ENDCOLOR} ${WHITE}$1${ENDCOLOR}\n"
+}
+
+#================================================================
+# FUNCTION: bold_info
+# DESCRIPTION:
+#     Prints bold info message using the provided parameter.
+# PARAMETERS:
+#     $1 - A message
+# RETURNS:
+#     None
+#================================================================
+bold_info() {
+	local message=$1
+	printf "${WHITE}[${ENDCOLOR}${CYAN}$current_time${ENDCOLOR}${WHITE}]${ENDCOLOR} ${WHITE}[${ENDCOLOR}${BOLD_GREEN}INFO${ENDCOLOR}${WHITE}]${ENDCOLOR} ${BOLD_WHITE}$1${ENDCOLOR}\n"
+}
+
+#================================================================
+# FUNCTION: success
+# DESCRIPTION:
+#     Prints success message using the provided parameter.
+#     $1 - A message
+# RETURNS:
+#     None
+#================================================================
+success() {
+	local message=$1
+	printf "${WHITE}[${ENDCOLOR}${CYAN}$current_time${ENDCOLOR}${WHITE}]${ENDCOLOR} ${WHITE}[${ENDCOLOR}${BOLD_GREEN}SUCCESS${ENDCOLOR}${WHITE}]${ENDCOLOR} ${BOLD_WHITE}$1${ENDCOLOR}\n"
+}
+
+#================================================================
+# FUNCTION: warning
+# DESCRIPTION:
+#     Prints warning message using the provided parameter.
+# PARAMETERS:
+#     $1 - A message
+# RETURNS:
+#     None
+#================================================================
+warning() {
+	local message=$1
+	printf "${WHITE}[${ENDCOLOR}${CYAN}$current_time${ENDCOLOR}${WHITE}]${ENDCOLOR} ${WHITE}[${ENDCOLOR}${YELLOW}WARNING${ENDCOLOR}${WHITE}]${ENDCOLOR} ${WHITE}$1${ENDCOLOR}\n"
+}
+
+#================================================================
+# FUNCTION: bold_warning
+# DESCRIPTION:
+#     Prints bold warning message using the provided parameter.
+# PARAMETERS:
+#     $1 - A message
+# RETURNS:
+#     None
+#================================================================
+bold_warning() {
+	local message=$1
+ 	printf "${WHITE}[${ENDCOLOR}${CYAN}$current_time${ENDCOLOR}${WHITE}]${ENDCOLOR} ${WHITE}[${ENDCOLOR}${BOLD_YELLOW}WARNING${ENDCOLOR}${WHITE}]${ENDCOLOR} ${BOLD_WHITE}$1${ENDCOLOR}\n"
+}
+
+#================================================================
+# FUNCTION: error
+# DESCRIPTION:
+#     Prints error message using the provided parameter.
+# PARAMETERS:
+#     $1 - A message
+# RETURNS:
+#     None
+#================================================================
+error() {
+	local message=$1
+	printf "${WHITE}[${ENDCOLOR}${CYAN}$current_time${ENDCOLOR}${WHITE}]${ENDCOLOR} ${WHITE}[${ENDCOLOR}${RED}ERROR${ENDCOLOR}${WHITE}]${ENDCOLOR} ${WHITE}$1${ENDCOLOR}\n"
+}
+
+#================================================================
+# FUNCTION: bold_error
+# DESCRIPTION:
+#     Prints bold error message using the provided parameter.
+# PARAMETERS:
+#     $1 - A message
+# RETURNS:
+#     None
+#================================================================
+bold_error() {
+	local message=$1
+	printf "${WHITE}[${ENDCOLOR}${CYAN}$current_time${ENDCOLOR}${WHITE}]${ENDCOLOR} ${WHITE}[${ENDCOLOR}${BOLD_RED}ERROR${ENDCOLOR}${WHITE}]${ENDCOLOR} ${BOLD_WHITE}$1${ENDCOLOR}\n"
+}
+
+#================================================================
+# FUNCTION: usage
+# DESCRIPTION:
+#     Prints default usage information.
+# PARAMETERS:
+#     None
+# RETURNS:
+#     None
+#================================================================
+usage() {
+	echo "Linux-From-Scratch Installer"
+  	echo " "
+	echo "Usage: ./LFSInstaller.sh [options]"
+	exit 0
+}
+
+
+#================================================================
+# FUNCTION: mount
+# DESCRIPTION:
+#     Mounts LFS drive
+# PARAMETERS:
+#     None
+# RETURNS:
+#     None
+#================================================================
+mount() {
+	if grep -qs /mnt/lfs /proc/mounts; then
+		echo "Mounted /dev/sda1 to $LFS"
+	else
+		echo "Currently not mounted."
+		echo "Proceeds to mount /dev/sda1 to $LFS."
+		mkdir -pv $LFS
+		mount -v -t ext4 /dev/sda1 $LFS
+		mkdir -v $LFS/home
+		mount -v -t ext4 /dev/sda1 $LFS/home
+		echo "Mounted /dev/sda1 to $LFS."
+	fi
+}
+
+#================================================================
+# FUNCTION: unmount
+# DESCRIPTION:
+#     Unmounts LFS drive
+# PARAMETERS:
+#     None
+# RETURNS:
+#     None
+#================================================================
+unmount() {
+	echo "Unmounting the virtual file systems"	
+	umount -v $LFS/dev/pts
+	mountpoint -q $LFS/dev/shm && umount -v $LFS/dev/shm
+	umount -v $LFS/dev
+	umount -v $LFS/run
+	umount -v $LFS/proc
+	umount -v $LFS/sys
+
+	echo "Unmounting the LFS file system"	
+	umount -v $LFS/home
+	umount -v $LFS
+
+	umount -v $LFS
+}
+
+#================================================================
+# FUNCTION: force_unmount
+# DESCRIPTION:
+#     Unmount by force of all LFS drives
+# PARAMETERS:
+#     None
+# RETURNS:
+#     None
+#================================================================
+unmount() {
+	echo "Unmounting the virtual file systems"	
+	umount -f $LFS/dev/pts
+	mountpoint -q $LFS/dev/shm && umount -f $LFS/dev/shm
+	umount -f $LFS/dev
+	umount -f $LFS/run
+	umount -f $LFS/proc
+	umount -f $LFS/sys
+
+	echo "Unmounting the LFS file system"	
+	umount -f $LFS/home
+	umount -f $LFS
+
+	umount -f $LFS
+}
+
+#================================================================
+# FUNCTION: chroot
+# DESCRIPTION:
+#     Enters chroot environment as an isolated machine
+# PARAMETERS:
+#     None
+# RETURNS:
+#     None
+#================================================================
+chroot() {
+
+}
+
+#================================================================
+# FUNCTION: version
+# DESCRIPTION:
+#     Selects LFS Release Build version before installation
+# PARAMETERS:
+#     None
+# RETURNS:
+#     None
+#================================================================
+version() {
+
+}
+
+#================================================================
+# FUNCTION: install
+# DESCRIPTION:
+#     Installs LFS Release Build Version
+# PARAMETERS:
+#     None
+# RETURNS:
+#     None
+#================================================================
+install() {
+
+}
+
+#================================================================
+# FUNCTION: help
+# DESCRIPTION:
+#     Prints default help information.
+# PARAMETERS:
+#     None
+# RETURNS:
+#     None
+#================================================================
+help() {
+	echo "Linux-From-Scratch Installer"
+	echo " "
+	echo "Usage: ./LFSInstaller.sh [options]"
+	echo "It is recommended to run this as a root user"
+	echo " "
+	echo "Options:"
+  	printf "  ${BOLD}--usage${ENDCOLOR}						Show usage information\n"
+	printf "  ${BOLD}-h, --help${ENDCOLOR}						Show help message\n"
+	echo " "
+	exit 0
+}
+ 
+VARIABLE=""
+VERBOSE=false
+ 
+while [[ "$#" -gt 0 ]]; do
+	case  in
+		-u|--usage) usage ;;
+		-h|--help) help ;;
+		-v|--variable) VARIABLE="$2"; shift ;;
+		--verbose) VERBOSE=true ;;
+		*) error "Unknown parameter passed: "; exit 1 ;;
+	esac
+	shift
+done
